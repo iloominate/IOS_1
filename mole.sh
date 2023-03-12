8adce323a4db5a8ba383fce5fd9489a2fe8beb4b
Aliaksei <xklima34@vutbr.cz>
2023-03-12 23:19:13 +0100

    Added script mole.sh

---
+++ mole.sh
@@ -0,0 +1,66 @@
+POSIXLY_CORRECT=yes
+#!/bin/bash
+
+LIST=""
+SECRETLOG=""
+
+DIRECTORY="$PWD"
+FILE=""
+
+group=""
+mode=""
+before=""
+after=""
+workDir=""
+
+usage() {
+  echo "Usage: $0 [-h] [-g GROUP] [-m] [-b DATE] [-a DATE] [FILTERS] [DIRECTORY]" 1>&2
+}
+
+if [[ $1 == "list" ]]; then
+  LIST=true
+  shift 1
+elif [[ $1 == "secret-log" ]]; then
+  SECRETLOG=true
+  shift 1
+fi
+
+usage=""
+group=""
+mode=""
+before=""
+after=""
+workDir=""
+
+while getopts ":g:b:a:mh" opt; do
+  case ${opt} in
+    h ) usage;;
+    g ) group="$OPTARG";;
+    m ) mode="true";;
+    b ) before="$OPTARG";;
+    a ) after="$OPTARG";;
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2;;
+    : ) 
+      echo "Option -$OPTARG requires an argument." 1>&2; usage;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [[ $# -gt 0 ]]; then
+  if [ -d "$1" ]; then
+    DIRECTORY="$1"
+  elif [ -f "$1" ]; then
+    FILE="$1"
+  else 
+    echo "Invalid argument: $1" 1>&2
+  fi
+fi
+
+echo "FILE: $FILE"
+echo "Directory: $DIRECTORY"
+echo "a- $after"
+echo "b- $before"
+echo "Secret log: $SECRETLOG"
+echo "LIST: $LIST"
+
